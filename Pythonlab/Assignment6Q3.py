ca038b434b3293491f2c5dcfcf2f003f51dc2dab
GedeonMatabaro <mugishomatgedeon@gmail.com>
2022-04-06 12:26:02 +0530

    okay man

--- Pythonlab/Assignment6Q3.py
+++ Pythonlab/Assignment6Q3.py
@@ -1,6 +1,7 @@
 import array
+Size=int(input("Enter the size of the array:"))
 #Insert elemnt
-def insert(A,Size):
+def insert(A):
     c=int(input("Enter the position to insert the element\n"))
     b=int(input("Enter the value to be inserted\n"))
     if(c>Size):
@@ -10,14 +11,14 @@ def insert(A,Size):
         for i in range(Size-1,c-1,-1):
             A[i+1]=A[i]
         A[c]=b
-Size=int(input("Enter the size of the array:"))
+        Size+=1
 A=array.array('i',range(20))
 for i in range(0, Size):
     A[i]=int(input(":-> "))
 print("The array elements are")
 for i in range(0,Size):
     print(A[i],end=" |")
-insert(A,Size)
+insert(A)
 print("The array elements after inserted are")
 for i in range(0,Size):
     print(A[i],end=" |")
